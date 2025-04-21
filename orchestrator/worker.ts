8782446b16172e47df57a01968896127e3ff4960
Francisco Lopez <franciscoj.lopezv@gmail.com>
2025-04-20 22:06:24 -0400

    Add temporal cloud secure connection

--- orchestrator/worker.ts
+++ orchestrator/worker.ts
@@ -1,10 +1,19 @@
 import { NativeConnection, Worker } from '@temporalio/worker';
+import fs from 'fs';
 
 async function run() {
   const temporalAddress = process.env.TEMPORAL_ADDRESS || 'localhost:7233';
-
-  const connection = await NativeConnection.connect({ address: temporalAddress });
   
+  const connection = await NativeConnection.connect({
+    address: temporalAddress,
+    tls: {
+      clientCertPair: {
+        crt: fs.readFileSync('/etc/ssl/client/client.crt'),
+        key: fs.readFileSync('/etc/ssl/client/client.key'),
+      }
+    },
+  });
+
   const worker = await Worker.create({
     connection,
     workflowsPath: require.resolve('./workflows/dynamic-orchestrator.workflow.ts'),
