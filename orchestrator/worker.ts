d52bb31710eff752521e7120569bb3383dd287a3
Francisco Lopez <franciscoj.lopezv@gmail.com>
2025-04-20 23:27:57 -0400

    add CI to generate the orchestrator image

--- orchestrator/worker.ts
+++ orchestrator/worker.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 async function run() {
   const temporalAddress = process.env.TEMPORAL_ADDRESS || 'localhost:7233';
+  const temporalNamespace = process.env.TEMPORAL_NAMESPACE || 'localhost:7233';
   
   const connection = await NativeConnection.connect({
     address: temporalAddress,
@@ -16,6 +17,7 @@ async function run() {
 
   const worker = await Worker.create({
     connection,
+    namespace: temporalNamespace,
     workflowsPath: require.resolve('./workflows/dynamic-orchestrator.workflow.ts'),
     taskQueue: 'orchestrator'
   });
